324490e4886cc2c57a8112a1c56487474c03e9ad
Shiyao Zhang <zhangshiyao_ZSY@outlook.com>
2026-06-01 20:59:23 +0800

    Initial Commit

---
+++ agent comm ios/agent_comm_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  agent_comm_iosApp.swift
+//  agent comm ios
+//
+//  Created by Shiyao Zhang on 01/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct agent_comm_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
